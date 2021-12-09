2b9bb58827b0c8b5571be9074d522f546c4e077a
matszach <lkaszubowski@gmail.com>
2021-12-09 02:22:30 +0100

    sample images

--- src/components/ArtPiece/ArtPiece.tsx
+++ src/components/ArtPiece/ArtPiece.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './ArtPiece.sass'
 
 function ArtPiece(props) {
+  const { cols, image, key } = props.piece
   return (
-    <div className='ArtPiece'>
-      {props.piece.key}
-    </div>
+    <div className={`ArtPiece cols-${cols}`}>
+      <img src={`./art/${image}`} alt={key} />
+      <label>{key}</label>
+    </div> 
   )
 }
 
--- src/components/ArtSection/ArtSection.tsx
+++ src/components/ArtSection/ArtSection.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ArtPiece from '../ArtPiece/ArtPiece'
 import './ArtSection.sass'
 
 function ArtSection(props) {
+
+  const [ expanded, setExpanded ] = useState(false)
+
+  const toggle = event => {
+    setExpanded(!expanded)
+  }
+
   return (
-    <div className='ArtSection'>
-      <p>{props.section.key}</p>
-      {props.section.pieces.map((p, i) => <ArtPiece piece={p} key={i}></ArtPiece>)}
+    <div className={`ArtSection ${expanded ? 'expanded' : ''}`}>
+      <p onClick={toggle}>{props.section.key}</p>
+      <div className='art-pieces-home'>
+        {props.section.pieces.map((p, i) => <ArtPiece piece={p} key={i}></ArtPiece>)}
+      </div>
     </div>
   )
 }
