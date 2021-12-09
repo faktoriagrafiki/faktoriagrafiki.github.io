40d74f184949d325e6272fcc8bc85bdc2c6ed24f
matszach <lkaszubowski@gmail.com>
2021-12-09 22:22:31 +0100

    email

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.sass'
 function IconLink(props) {
   const [ src, alt, url ] = props.args
   return (
-    <a className='icon-url' href={url} target='_blank'>
+    <a className='icon-link' href={url} target='_blank' rel='noopener noreferrer'>
       <img src={src} alt={alt}></img>
     </a>
   )
@@ -12,18 +12,23 @@ function IconLink(props) {
 
 function Footer() {
 
-  const [ urls, setUrls ] = useState({} as any)
+  const [ config, setConfig ] = useState({} as any)
 
   useEffect(() => {
-    fetch('./config.json').then(r => r.json()).then(r => setUrls(r.urls))
+    fetch('./config.json').then(r => r.json()).then(setConfig)
   }, [])
 
   return (
     <footer className='Footer'>
-      <IconLink args={['./icons/facebook.png', 'facebook', urls.facebook]}></IconLink>
-      <IconLink args={['./icons/linkedin.png', 'linkedin', urls.linkedin]}></IconLink>
-      <IconLink args={['./icons/instagram.png', 'instagram', urls.instagram]}></IconLink>
-      <IconLink args={['./icons/artstation.png', 'artstation', urls.artstation]}></IconLink>
+      <div className='icon-link-block'>
+        <IconLink args={['./icons/facebook.png', 'facebook', config.urls?.facebook]}></IconLink>
+        <IconLink args={['./icons/linkedin.png', 'linkedin', config.urls?.linkedin]}></IconLink>
+        <IconLink args={['./icons/instagram.png', 'instagram', config.urls?.instagram]}></IconLink>
+        <IconLink args={['./icons/artstation.png', 'artstation', config.urls?.artstation]}></IconLink>
+      </div>
+      <div className='email-block'>
+        <label>{config.email}</label>
+      </div>
     </footer>
   )
 }
