46c17b00f59ffbd168dcff92743d5bec76713a86
matszach <lkaszubowski@gmail.com>
2021-12-11 17:30:12 +0100

    language url arg

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import i18next from 'i18next'
 import { useTranslation } from 'react-i18next'
 import './Nav.sass'
+import { getParam, setParam } from '../../util/url'
 
 function Nav() {
 
   const { t } = useTranslation('common')
+
+  useEffect(() => {
+    const lang = getParam('l')
+    if (!!lang) {
+      setTimeout(() => i18next.changeLanguage(lang))
+    }
+  }, [])
+
 
   const toBottom = () => window.scrollTo({
     top: document.body.scrollHeight,
@@ -14,6 +23,7 @@ function Nav() {
 
   const toLang = key => {
     return e => {
+      setParam('l', key)
       i18next.changeLanguage(key)
     }
   }
