612701f147ce055b69feb3901791074f314cafb3
Lukasz Kaszubowski <lukasz_kaszubowski@epam.com>
2021-12-20 15:58:06 +0100

    mobile test 2

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -7,8 +7,8 @@ function Background() {
       <span className='circle a'></span>
       <span className='circle b'></span>
       <span className='circle c'></span>
-      {/* <span className='circle d'></span>
-      <span className='circle e'></span> */}
+      <span className='circle d'></span>
+      <span className='circle e'></span>
       <span className='circle f'></span>
     </div>
   )
