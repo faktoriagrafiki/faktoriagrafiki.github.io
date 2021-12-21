796153725265137852f51538092d505487a11073
Lukasz Kaszubowski <lukasz_kaszubowski@epam.com>
2021-12-21 16:45:02 +0100

    site title overriden in the helmet component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import Content from './components/Content/Content'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
+import { Helmet } from 'react-helmet'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t } = useTranslation('common')
   return (
     <div className='App'>
+      <Helmet>
+        <title>{t('meta.title')}</title>
+      </Helmet>
       <Background></Background>
       <Nav></Nav>
       <Header></Header>
